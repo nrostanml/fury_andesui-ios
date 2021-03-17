5a3a3d03434d7990602467264a506d4cd4944ae5
Ariel Goldfryd <ariel.goldfryd@mercadolibre.com>
2021-03-17 11:46:03 -0300

    Move folder

---
+++ LibraryComponents/Classes/Core/AndesAutocompleteTextField/Hierarchy/AndesAutocompleteTextFieldProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  
+//  AndesAutocompleteTextFieldProtocol.swift
+//  AndesUI
+//
+//  Created by Ariel Goldfryd on 17/03/2021.
+//
+//
+
+import Foundation
+
+internal protocol AndesAutocompleteTextFieldHierarchyProtocol {
+    var backgroundColor: UIColor { get }
+}
---
+++ LibraryComponents/Classes/Core/AndesAutocompleteTextField/Type/AndesAutocompleteTextFieldTypeError.swift
@@ -0,0 +1,14 @@
+//
+//  
+//  AndesAutocompleteTextFieldTypeError.swift
+//  AndesUI
+//
+//  Created by Ariel Goldfryd on 17/03/2021.
+//
+//
+
+import Foundation
+
+class AndesAutocompleteTextFieldTypeError: AndesAutocompleteTextFieldTypeProtocol {
+    var textColor: UIColor = UIColor.Andes.red500
+}
